1855a8eee1594df9faee13d406b467ed783425b7
visualstudio <visualstudio@230-PC40>
2019-02-07 15:22:01 -0500

    3:10

--- c0728853/Program.cs
+++ c0728853/Program.cs
@@ -4,12 +4,63 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace c0728853
+namespace _2019WCSD3354WEEK05
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var a = new testques2();
+            a.Play();
         }
     }
+
+    class testques2
+    {
+        public int myfavvari = 0;
+        public void Play()
+        {
+            //write a for loop
+
+            for (; my();)
+            {
+                if (myfavvari <= 10)
+                    Console.WriteLine("you are fool");
+                break;
+            }
+            Console.WriteLine("shit for loop again");
+        }
+        public bool my()
+        {
+            myfavvari++;
+            return true;
+        }
+    }
+
+    class dog
+    {
+        public dog(string name, string breed)
+        {
+            dog_name = name;
+            dog_breed = breed;
+        }
+        public string dog_name;
+        public string dog_breed;
+
+        public dog next dog;
+public dog prev_dog;
+
+    }
+    class birthday_party
+    {
+        public dog peanut;
+        public dog fifi;
+        public dog clarence;
+        public dog roy;
+
+        public dog head;
+        public dog tail;
+        public dog temporary;
+    }
 }
+
